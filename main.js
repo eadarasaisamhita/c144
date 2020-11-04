c9356d5872ea36981b61d3688c941c1235b6418b
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-11-04 16:41:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-function preload()
-{
+function preload(){
   
 }
 
 function setup() 
 {
-  canvas=createCanvas(300,300);
+  canvas = createCanvas(300, 300);
   canvas.center();
+  video = createCapture(VIDEO);
+  vidoe.size(300, 300);
+  video.hide();
+
+  postNet= ml5.postNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) 
+{
+  if(results.length>0){
+    console.log(results);
+    console.log("nose x="+results[0].pose.nose.x);
+    console.log("nose y="+results[0].pose.nose.y);
+  }
+}
+
 function draw() 
 {
-  
+  image(video,0 ,0 ,300 ,300);
 }
 
 function take_snapshot() 
